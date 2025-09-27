9682d7a3aed69d9d7ff83feb76b3f9e2cceae9da
Loryyyy007 <lorenzobarbero.07@gmail.com>
2025-09-27 10:36:41 +0200

    Fixed a problem regarding item actions

--- src/main/kotlin/me/loryyyy/loryPaperExtensions/item_actions/ItemActionManager.kt
+++ src/main/kotlin/me/loryyyy/loryPaperExtensions/item_actions/ItemActionManager.kt
@@ -1,6 +1,7 @@
 package me.loryyyy.loryPaperExtensions.item_actions
 
 import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
@@ -8,6 +9,7 @@ import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
+import java.lang.System.currentTimeMillis
 
 object ItemActionManager : Listener {
 
@@ -15,14 +17,21 @@ object ItemActionManager : Listener {
         Bukkit.getPluginManager().registerEvents(this, plugin)
     }
 
+    private val lastDropEventForPlayer = mutableMapOf<Player, Long>()
+    private const val DROP_INTERACT_THRESHOLD = 50L
+
     @EventHandler
     fun on(event: PlayerInteractEvent) {
+        if(currentTimeMillis() < ((lastDropEventForPlayer[event.player] ?: 0L) + DROP_INTERACT_THRESHOLD)) return
+
         val item = event.item ?: return
         ItemActionsRegistry.handleInteraction(item, event)
     }
 
     @EventHandler
     fun on(event: PlayerDropItemEvent) {
+        lastDropEventForPlayer[event.player] = currentTimeMillis()
+
         val item = event.itemDrop.itemStack
         ItemActionsRegistry.handleInteraction(item, event)
     }
